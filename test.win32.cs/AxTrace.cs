b9a8ee9cc0a944381124fbd3ff0a3d9312de72f2
Jin Chao <thejinchao@gmail.com>
2014-12-03 23:48:06 +0800

    完善C#测试

--- test.win32.cs/AxTrace.cs
+++ test.win32.cs/AxTrace.cs
@@ -5,79 +5,111 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 
-static class Axia
+namespace com.thecodeway
 {
-    [DllImport("Kernel32.dll")]
-    static extern IntPtr LoadLibrary(string lpFileName);
-
-    [DllImport("Kernel32.dll")]
-    static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
-
-    delegate int AxTrace_Init();
-    delegate int AxTrace_InsertLogA(int idWindow, int idStyle, int isUTF8, string stringBuf);
-    delegate int AxTrace_InsertLogW(int idWindow, int idStyle, short[] stringBuf);
-    delegate int AxTrace_WatchValue(int idWindow, int idStyle, int valueType, string valueName, int valueNameLength, byte[] value);
-
-    class GLOBAL_DATA
+    public class AxTrace
     {
-        public AxTrace_Init funcInit;
-        public AxTrace_InsertLogA funcInsertLogA;
-        public AxTrace_InsertLogW funcInsertLogW;
-        public AxTrace_WatchValue funcWatchValue;
-        public bool init;
-        public bool initSuccess;
+        public static readonly int AX_INT8 = 0;
+        public static readonly int AX_UINT8 = 1;
+        public static readonly int AX_INT16 = 2;
+        public static readonly int AX_UINT16 = 3;
+        public static readonly int AX_INT32 = 4;
+        public static readonly int AX_UINT32 = 5;
+        public static readonly int AX_INT64 = 6;
+        public static readonly int AX_UINT64 = 7;
+        public static readonly int AX_FLOAT32 = 8;
+        public static readonly int AX_FLOAT64 = 9;
+        public static readonly int AX_STR_ACP = 10;
+        public static readonly int AX_STR_UTF8 = 11;
+        public static readonly int AX_STR_UTF16 = 12;
+        
+        [DllImport("axtrace")]
+        static extern void AxTrace_Init();
 
-        public GLOBAL_DATA()
+        [DllImport("axtrace")]
+        static extern void AxTrace_InsertLogA(int idWindow, int idStyle, int isUTF8, byte[] stringBuf);
+
+        [DllImport("axtrace")]
+        static extern void AxTrace_InsertLogW(int idWindow, int idStyle, short[] stringBuf);
+
+        [DllImport("axtrace")]
+        static extern void AxTrace_WatchValue(int idWindow, int idStyle, int valueType, byte[] valueName, int valueNameLength, byte[] value);
+
+        static public void Trace(string format, params object[] args)
         {
-            funcInit = null;
-            funcInsertLogA = null;
-            funcInsertLogW = null;
-            funcWatchValue = null;
-            init = false;
-            initSuccess = false;
+            string final = System.String.Format(format, args);
+            AxTrace_InsertLogA(0, 0, 1, System.Text.Encoding.UTF8.GetBytes(final));
         }
-    };
-
-    static private object m_lockGlobalData = new object();
-    static private GLOBAL_DATA m_theGlobalData = new GLOBAL_DATA();
-
-    static private bool _LoadGlobalData()
-    {
-        if (m_theGlobalData.init) return m_theGlobalData.initSuccess;
-
-        lock (m_lockGlobalData)
+        static public void TraceEx(int idwindow, int idStyle, string format, params object[] args)
         {
-            m_theGlobalData.init = true;
-            m_theGlobalData.initSuccess = false;
-
-            IntPtr hModule = LoadLibrary("axtrace.dll");
-            if (hModule == IntPtr.Zero) return false;
-
-            IntPtr addrInit = GetProcAddress(hModule, "AxTrace_Init");
-            IntPtr addrInsertLogA = GetProcAddress(hModule, "AxTrace_InsertLogA");
-            IntPtr addrInsertLogW = GetProcAddress(hModule, "AxTrace_InsertLogW");
-            IntPtr addrWatchValue = GetProcAddress(hModule, "AxTrace_WatchValue");
-            if (addrInit == IntPtr.Zero || addrInsertLogA == IntPtr.Zero || addrInsertLogW == IntPtr.Zero || addrWatchValue == IntPtr.Zero) return false;
-
-            m_theGlobalData.funcInit = (AxTrace_Init)Marshal.GetDelegateForFunctionPointer(addrInit, typeof(AxTrace_Init));
-            m_theGlobalData.funcInsertLogA = (AxTrace_InsertLogA)Marshal.GetDelegateForFunctionPointer(addrInsertLogA, typeof(AxTrace_InsertLogA));
-            m_theGlobalData.funcInsertLogW = (AxTrace_InsertLogW)Marshal.GetDelegateForFunctionPointer(addrInsertLogA, typeof(AxTrace_InsertLogW));
-            m_theGlobalData.funcWatchValue = (AxTrace_WatchValue)Marshal.GetDelegateForFunctionPointer(addrWatchValue, typeof(AxTrace_WatchValue));
-            m_theGlobalData.initSuccess = true;
+            string final = System.String.Format(format, args);
+            AxTrace_InsertLogA(idwindow, idStyle, 1, System.Text.Encoding.UTF8.GetBytes(final));
         }
 
-        return m_theGlobalData.initSuccess;
+        static private byte[] _getStringWithBlank(string value)
+        {
+            byte[] _value = System.Text.Encoding.UTF8.GetBytes(value);
+            byte[] _value_with_blank = new byte[_value.Length + 1];
+            _value.CopyTo(_value_with_blank, 0);
+            _value_with_blank[_value.Length] = 0;
+            return _value_with_blank;
+        }
+
+        static public void Value(string name, sbyte value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_INT8, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, byte value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_UINT8, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, short value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_INT16, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, ushort value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_UINT16, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, int value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_INT32, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, uint value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_UINT32, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, long value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_INT64, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, ulong value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_UINT64, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, float value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_FLOAT32, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, double value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_FLOAT64, _name, _name.Length, BitConverter.GetBytes(value));
+        }
+        static public void Value(string name, string value)
+        {
+            byte[] _name = _getStringWithBlank(name);
+            AxTrace_WatchValue(0, 0, AX_STR_UTF8, _name, _name.Length, _getStringWithBlank(value));
+        }
     }
 
-    static public void AxTrace(int idwindow, int idStyle, string value)
-    {
-        if (!_LoadGlobalData()) return;
-        m_theGlobalData.funcInsertLogA(idwindow, idStyle, 0, value);
-    }
-    static public void AxTrace_UTF8(int idwindow, int idStyle, string value)
-    {
-        if (!_LoadGlobalData()) return;
-        m_theGlobalData.funcInsertLogA(idwindow, idStyle, 1, value);
-    }
 }
-
