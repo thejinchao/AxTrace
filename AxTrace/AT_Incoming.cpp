47be6b29bde7bd2746e3826a0046fc7e6bbcaf2f
Jin Chao <thejinchao@gmail.com>
2015-03-11 19:11:41 +0800

    继续简化代码，去掉incoming模块中不再使用的代码

--- AxTrace/AT_Incoming.cpp
+++ AxTrace/AT_Incoming.cpp
@@ -16,30 +16,27 @@ namespace AT3
 
 //--------------------------------------------------------------------------------------------
 Incoming::Incoming()
-	: m_nListenPort(DEFAULT_PORT)	
-	, m_hReceiveThread(0)
-	, m_opPull(0)
-	, m_opQuit(0)
-	, QUIT_SIGNAL_PORT("inproc://quit-signal")
+	: m_server(0)
 {
 }
 
 //--------------------------------------------------------------------------------------------
 Incoming::~Incoming()
 {
+	if (m_server){
+		delete m_server;
+	}
 }
 
 //--------------------------------------------------------------------------------------------
-bool Incoming::init_cyclone(void)
+bool Incoming::init(void)
 {
 	cyclone::Address address(DEFAULT_PORT, false);
-	m_server = new cyclone::TcpServer(address, this);
+	m_server = new cyclone::TcpServer(this);
 
 	m_server->set_message_callback(_cyclone_message_callback_entry);
 
-	m_server->start(2);
-
-	return true;
+	return m_server->start(address, false, 2);
 }
 
 //--------------------------------------------------------------------------------------------
@@ -85,124 +82,10 @@ void Incoming::_cyclone_message_callback(cyclone::Connection* conn)
 }
 
 //--------------------------------------------------------------------------------------------
-void Incoming::closeListen_cyclone(void)
-{
-	delete m_server;
-}
-
-//--------------------------------------------------------------------------------------------
-bool Incoming::init(void)
-{
-	return init_cyclone();
-	assert(m_hReceiveThread==0);
-	const Config* config = System::getSingleton()->getConfig();
-
-	//create pull port
-	if (!_createPullPort()) return false;
-
-	//create quit op port
-	m_opQuit = zmq_socket(System::getSingleton()->getZeroMQ(), ZMQ_PULL);
-	if (0 != zmq_bind(m_opQuit, QUIT_SIGNAL_PORT.c_str())) return false;
-
-	//begin listen thread
-	unsigned int threadID;
-	m_hReceiveThread = (HANDLE)::_beginthreadex(0, 0, 
-		__threadEntry, this, THREAD_QUERY_INFORMATION, &threadID);
-	if(m_hReceiveThread == 0) 
-	{
-		zmq_close(m_opPull);
-		return false;
-	}
-
-	return true;
-}
-
-//--------------------------------------------------------------------------------------------
-unsigned int Incoming::__threadEntry(void* param)
-{
-	return ((Incoming*)param)->_threadEntry();
-}
-
-//--------------------------------------------------------------------------------------------
-bool Incoming::_createPullPort(void)
-{
-	void* ctx = System::getSingleton()->getZeroMQ();
-
-	//try create zmp port
-	void* port = zmq_socket(System::getSingleton()->getZeroMQ(), ZMQ_PULL);
-
-	char temp[MAX_PATH]={0};
-	StringCchPrintfA(temp, MAX_PATH, "tcp://*:%d", m_nListenPort);
-	if(0==zmq_bind(port, temp))
-	{
-		m_opPull = port;
-		m_strListenPort = temp;
-		return true;
-	}
-	zmq_close(port);
-	return false;
-}
-
-//--------------------------------------------------------------------------------------------
-unsigned int Incoming::_threadEntry(void)
-{
-	//work loop...
-	while(true)
-	{
-		zmq_msg_t recv_command;
-		zmq_msg_init(&recv_command);
-
-		//receive
-		zmq_pollitem_t wait_items[] = {
-			{ m_opQuit, 0, ZMQ_POLLIN, 0 },
-			{ m_opPull, 0, ZMQ_POLLIN, 0 }
-		};
-
-		//wait receive command or quit signal
-		zmq_poll(wait_items, 2, -1);
-
-		//receive quit signal, quit!
-		if ((wait_items[0].revents & ZMQ_POLLIN)) break;
-
-		//receive message
-		if ((wait_items[1].revents & ZMQ_POLLIN) && zmq_msg_recv(&recv_command, m_opPull, 0)>0)
-		{
-			if(System::getSingleton()->getConfig()->getCapture())
-			{
-				SYSTEMTIME tTime;
-				GetLocalTime(&tTime);
-
-				System::getSingleton()->getMessageQueue()->insertMessage((const char*)zmq_msg_data(&recv_command), zmq_msg_size(&recv_command), &tTime);
-			}
-		}
-		zmq_msg_close(&recv_command);
-	}
-
-	_endthread();
-	return 0;
-}
-
-//--------------------------------------------------------------------------------------------
 void Incoming::closeListen(void)
 {
-	//push a dummy msg to let incoming loop quit
-	void* s = zmq_socket(System::getSingleton()->getZeroMQ(), ZMQ_PUSH);
-	zmq_connect(s, QUIT_SIGNAL_PORT.c_str());
-	zmq_send(s, " ", 1, 0);
-	zmq_close(s);
-
-	//wait thred quit(max 1sec)
-	if(WAIT_TIMEOUT == WaitForSingleObject(m_hReceiveThread, 1000))
-	{
-		//destroy thread
-		::TerminateThread(m_hReceiveThread, 0);
-	}
-
-	//close zmq bind
-	zmq_unbind(m_opPull, m_strListenPort.c_str());
-
-	zmq_close(m_opPull);
-	zmq_close(m_opQuit);
+	//wait cyclone quit...
+	m_server->stop();
 }
 
 }
--- AxTrace/AT_Incoming.h
+++ AxTrace/AT_Incoming.h
@@ -20,40 +20,19 @@ namespace AT3
 class Incoming
 {
 public:
+	//default listen port is 1978 :)
+	enum { DEFAULT_PORT = 1978 };
+
 	/** create receive thread */
 	bool init(void);
-	bool init_cyclone(void);
 	/** try close receive thread*/
 	void closeListen(void);
-	void closeListen_cyclone(void);
 
 private:
 	cyclone::TcpServer* m_server;
 
 	static void _cyclone_message_callback_entry(cyclone::TcpServer* server, cyclone::Connection* conn);
 	void _cyclone_message_callback(cyclone::Connection* conn);
-
-private:
-	HANDLE		m_hReceiveThread;	//!< receive thread handle
-	int			m_nListenPort;		//!< tcp port
-	std::string	m_strListenPort;	//!< listen port(string format)
-
-	//default listen port is 1978 :)
-	enum { DEFAULT_PORT=1978 };
-	void*		m_opPull;			//!< zmp port to recive msg
-
-	//force quit signal
-	void*		m_opQuit;
-	
-	const std::string QUIT_SIGNAL_PORT;
-
-private:
-	/** thread entry function */
-	static unsigned int __stdcall __threadEntry(void* param);
-	/** thread function	*/
-	unsigned int _threadEntry(void);
-	/** create zmp port*/
-	bool _createPullPort(void);
 
 public:
 	Incoming();
