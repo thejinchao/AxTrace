f58d33b30da8e6000c5662fe13d770ccffa48a84
Jin Chao <thejinchao@gmail.com>
2015-03-12 19:17:22 +0800

    初步恢复cs部分功能，并使用cmake构建c#工程

--- test.cs/AxTrace.cs
+++ test.cs/AxTrace.cs
@@ -4,96 +4,175 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System.Net;
+using System.Net.Sockets;
 
 namespace com.thecodeway
 {
     public class AxTrace
     {
-        public static readonly int AX_INT8 = 0;
-        public static readonly int AX_UINT8 = 1;
-        public static readonly int AX_INT16 = 2;
-        public static readonly int AX_UINT16 = 3;
-        public static readonly int AX_INT32 = 4;
-        public static readonly int AX_UINT32 = 5;
-        public static readonly int AX_INT64 = 6;
-        public static readonly int AX_UINT64 = 7;
-        public static readonly int AX_FLOAT32 = 8;
-        public static readonly int AX_FLOAT64 = 9;
-        public static readonly int AX_STR_ACP = 10;
-        public static readonly int AX_STR_UTF8 = 11;
-        public static readonly int AX_STR_UTF16 = 12;
+        /*---------------------------------------------------------------------------------------------*/
+        public static readonly uint AXT_TRACE = 0;
+        public static readonly uint AXT_DEBUG = 1;
+        public static readonly uint AXT_INFO = 2;
+        public static readonly uint AXT_WARN = 3;
+        public static readonly uint AXT_ERROR = 4;
+        public static readonly uint AXT_FATAL = 5;
+        public static readonly uint AXT_USERDEF = 10;
+
+        public static readonly int AXV_INT8 = 0;
+        public static readonly int AXV_UINT8 = 1;
+        public static readonly int AXV_INT16 = 2;
+        public static readonly int AXV_UINT16 = 3;
+        public static readonly int AXV_INT32 = 4;
+        public static readonly int AXV_UINT32 = 5;
+        public static readonly int AXV_INT64 = 6;
+        public static readonly int AXV_UINT64 = 7;
+        public static readonly int AXV_FLOAT32 = 8;
+        public static readonly int AXV_FLOAT64 = 9;
+        public static readonly int AXV_STR_ACP = 10;
+        public static readonly int AXV_STR_UTF8 = 11;
+        public static readonly int AXV_STR_UTF16 = 12;
         
-        [DllImport("axtrace")]
-        static extern void AxTrace_Init(byte[] traceServer, int tracePort);
+        public static readonly byte AXTRACE_CMD_TYPE_TRACE = 1;
+        public static readonly byte AXTRACE_CMD_TYPE_VALUE = 2;
 
-        [DllImport("axtrace")]
-        static extern void AxTrace_InsertLogA(int idWindow, int idStyle, int isUTF8, byte[] stringBuf);
+        public static readonly int AXTRACE_MAX_TRACE_STRING_LENGTH = 0x8000;
 
-        [DllImport("axtrace")]
-        static extern void AxTrace_InsertLogW(int idWindow, int idStyle, short[] stringBuf);
+        //private static readonly ushort AX_CODEPAGE_ACP = 0;
+        private static readonly ushort AX_CODEPAGE_UTF8 = 1;
+        //private static readonly ushort AX_CODEPAGE_UTF16 = 2;
 
-        [DllImport("axtrace")]
-        static extern void AxTrace_WatchValue(int idWindow, int idStyle, int valueType, byte[] valueName, int valueNameLength, byte[] value);
-
-        static public void SetTraceServer(string ip, int port)
+        /*---------------------------------------------------------------------------------------------*/
+        /* AxTrace Global data  */
+        private class Context
         {
-            AxTrace_Init(System.Text.Encoding.UTF8.GetBytes(ip), port);
+            public bool inited;
+            public bool init_success;
+            public Socket socket;
+            public string server_addr;
+            public Int32 server_port;
+
+            public Context()
+            {
+                inited = false;
+                init_success = false;
+                server_addr = "127.0.0.1";
+                server_port = 1978;
+            }
+        };
+
+        [ThreadStatic]
+        static Context g_context = new Context();
+
+        /*---------------------------------------------------------------------------------------------*/
+        /* axtrace communication data struct*/
+        [Serializable]
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct axtrace_head_s
+        {
+            public ushort length;   /* length */
+            public byte flag;       /* magic flag, always 'A' */
+            public byte type;       /* command type AXTRACE_CMD_TYPE_* */
+            public uint pid;		/* process id*/
+            public uint tid;		/* thread id*/
+            public uint style;		/* trace style AXT_* */
+        };
+
+        [Serializable]
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct axtrace_trace_s
+        {
+            public axtrace_head_s head; /* common head */
+            public ushort code_page;	/* code page */
+            public ushort length;		/* trace string length */
+        };
+
+        static private Context _getContext()
+        {
+            if (g_context.inited) return g_context;
+
+            try
+            {
+                g_context.inited = true;
+
+                //try connect to server
+                IPEndPoint iep = new IPEndPoint(Dns.GetHostEntry(g_context.server_addr).AddressList[0], g_context.server_port);
+                g_context.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                g_context.socket.Connect(iep);
+
+                g_context.init_success = true;
+            }
+            catch (Exception)
+            {
+                g_context.init_success = false;
+            }
+            return g_context;
         }
 
-        static public void Trace(string format, params object[] args)
+        static public void Trace(uint style, string format, params object[] args)
         {
-            string final = System.String.Format(format, args);
-            AxTrace_InsertLogA(0, 0, 1, System.Text.Encoding.UTF8.GetBytes(final));
-        }
-        static public void TraceEx(int idwindow, int idStyle, string format, params object[] args)
-        {
-            string final = System.String.Format(format, args);
-            AxTrace_InsertLogA(idwindow, idStyle, 1, System.Text.Encoding.UTF8.GetBytes(final));
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            int headSize = Marshal.SizeOf(typeof(axtrace_trace_s));
+            byte[] buf = new byte[headSize + AXTRACE_MAX_TRACE_STRING_LENGTH];
+
+            string content = System.String.Format(format, args);
+            int content_length = content.Length + 1;
+            int final_length = Marshal.SizeOf(typeof(axtrace_trace_s)) + content_length;
+
+            axtrace_trace_s head = new axtrace_trace_s();
+            head.head.length = (ushort)(final_length);
+            head.head.flag = (byte)'A';
+            head.head.type = AXTRACE_CMD_TYPE_TRACE;
+            head.head.pid = 0;
+            head.head.tid = 0;
+            head.head.style = style;
+            head.code_page = AX_CODEPAGE_UTF8;
+            head.length = (ushort)content_length;
+
+            IntPtr headPtr = Marshal.AllocHGlobal(headSize);
+            Marshal.StructureToPtr(head, headPtr, false);
+            Marshal.Copy(headPtr, buf, 0, headSize);
+            System.Text.Encoding.UTF8.GetBytes(content).CopyTo(buf, headSize);
+            Marshal.FreeHGlobal(headPtr);
+
+            ctx.socket.Send(buf, 0, final_length, SocketFlags.DontRoute);
         }
 
         static public void Value(string name, sbyte value)
         {
-            AxTrace_WatchValue(0, 0, AX_INT8, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, byte value)
         {
-            AxTrace_WatchValue(0, 0, AX_UINT8, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, short value)
         {
-            AxTrace_WatchValue(0, 0, AX_INT16, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, ushort value)
         {
-            AxTrace_WatchValue(0, 0, AX_UINT16, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, int value)
         {
-            AxTrace_WatchValue(0, 0, AX_INT32, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, uint value)
         {
-            AxTrace_WatchValue(0, 0, AX_UINT32, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, long value)
         {
-            AxTrace_WatchValue(0, 0, AX_INT64, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, ulong value)
         {
-            AxTrace_WatchValue(0, 0, AX_UINT64, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, float value)
         {
-            AxTrace_WatchValue(0, 0, AX_FLOAT32, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, double value)
         {
-            AxTrace_WatchValue(0, 0, AX_FLOAT64, System.Text.Encoding.UTF8.GetBytes(name), name.Length, BitConverter.GetBytes(value));
         }
         static public void Value(string name, string value)
         {
-            AxTrace_WatchValue(0, 0, AX_STR_UTF8, System.Text.Encoding.UTF8.GetBytes(name), name.Length, System.Text.Encoding.UTF8.GetBytes(value));
         }
     }
 
