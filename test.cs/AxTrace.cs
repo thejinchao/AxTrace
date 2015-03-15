b38cb34ee5836c0bdc6e689368b70f0cc17edc4d
Jin Chao <thejinchao@gmail.com>
2015-03-15 08:27:49 +0800

    添加c#版本基本的axvalue功能

--- test.cs/AxTrace.cs
+++ test.cs/AxTrace.cs
@@ -20,24 +20,26 @@ namespace com.thecodeway
         public static readonly uint AXT_FATAL = 5;
         public static readonly uint AXT_USERDEF = 10;
 
-        public static readonly int AXV_INT8 = 0;
-        public static readonly int AXV_UINT8 = 1;
-        public static readonly int AXV_INT16 = 2;
-        public static readonly int AXV_UINT16 = 3;
-        public static readonly int AXV_INT32 = 4;
-        public static readonly int AXV_UINT32 = 5;
-        public static readonly int AXV_INT64 = 6;
-        public static readonly int AXV_UINT64 = 7;
-        public static readonly int AXV_FLOAT32 = 8;
-        public static readonly int AXV_FLOAT64 = 9;
-        public static readonly int AXV_STR_ACP = 10;
-        public static readonly int AXV_STR_UTF8 = 11;
-        public static readonly int AXV_STR_UTF16 = 12;
+        public static readonly uint AXV_INT8 = 0;
+        public static readonly uint AXV_UINT8 = 1;
+        public static readonly uint AXV_INT16 = 2;
+        public static readonly uint AXV_UINT16 = 3;
+        public static readonly uint AXV_INT32 = 4;
+        public static readonly uint AXV_UINT32 = 5;
+        public static readonly uint AXV_INT64 = 6;
+        public static readonly uint AXV_UINT64 = 7;
+        public static readonly uint AXV_FLOAT32 = 8;
+        public static readonly uint AXV_FLOAT64 = 9;
+        public static readonly uint AXV_STR_ACP = 10;
+        public static readonly uint AXV_STR_UTF8 = 11;
+        public static readonly uint AXV_STR_UTF16 = 12;
         
         public static readonly byte AXTRACE_CMD_TYPE_TRACE = 1;
         public static readonly byte AXTRACE_CMD_TYPE_VALUE = 2;
 
         public static readonly int AXTRACE_MAX_TRACE_STRING_LENGTH = 0x8000;
+        public static readonly int AXTRACE_MAX_VALUENAME_LENGTH	= 128;
+        public static readonly int AXTRACE_MAX_VALUE_LENGTH	= 1024;
 
         //private static readonly ushort AX_CODEPAGE_ACP = 0;
         private static readonly ushort AX_CODEPAGE_UTF8 = 1;
@@ -88,6 +90,16 @@ namespace com.thecodeway
             public ushort length;		/* trace string length */
         };
 
+        [Serializable]
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        private struct axtrace_value_s
+        {
+	        public axtrace_head_s	head;	/* common head */
+	        public uint	value_type;		    /* value type AXV_* */
+	        public ushort name_len;		    /* length of value name */
+            public ushort value_len;		/* length of value */
+        } ;
+
         static private Context _getContext()
         {
             if (g_context.inited) return g_context;
@@ -97,8 +109,9 @@ namespace com.thecodeway
                 g_context.inited = true;
 
                 //try connect to server
-                IPEndPoint iep = new IPEndPoint(Dns.GetHostEntry(g_context.server_addr).AddressList[0], g_context.server_port);
                 g_context.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                IPEndPoint iep = new IPEndPoint(IPAddress.Parse(g_context.server_addr), g_context.server_port);
                 g_context.socket.Connect(iep);
 
                 g_context.init_success = true;
@@ -119,6 +132,7 @@ namespace com.thecodeway
             byte[] buf = new byte[headSize + AXTRACE_MAX_TRACE_STRING_LENGTH];
 
             string content = System.String.Format(format, args);
+            //add '\0'
             int content_length = content.Length + 1;
             int final_length = Marshal.SizeOf(typeof(axtrace_trace_s)) + content_length;
 
@@ -136,43 +150,146 @@ namespace com.thecodeway
             Marshal.StructureToPtr(head, headPtr, false);
             Marshal.Copy(headPtr, buf, 0, headSize);
             System.Text.Encoding.UTF8.GetBytes(content).CopyTo(buf, headSize);
+            buf[final_length - 1] = 0; //add '\0'
             Marshal.FreeHGlobal(headPtr);
 
             ctx.socket.Send(buf, 0, final_length, SocketFlags.DontRoute);
         }
 
-        static public void Value(string name, sbyte value)
+        static private void _value(Context ctx, uint style, string name, uint value_type, byte[] valueBytes)
         {
+            byte[] nameBytes = System.Text.Encoding.UTF8.GetBytes(name);
+            int name_length = nameBytes.Length + 1; // add '\0'
+            int value_length = valueBytes.Length;
+
+            int headSize = Marshal.SizeOf(typeof(axtrace_value_s));
+            byte[] buf = new byte[headSize + AXTRACE_MAX_VALUENAME_LENGTH + AXTRACE_MAX_VALUE_LENGTH];
+            int final_length = Marshal.SizeOf(typeof(axtrace_value_s)) + name_length + value_length;
+
+            axtrace_value_s head = new axtrace_value_s();
+            head.head.length = (ushort)(final_length);
+            head.head.flag = (byte)'A';
+            head.head.type = AXTRACE_CMD_TYPE_VALUE;
+            head.head.pid = 0;
+            head.head.tid = 0;
+            head.head.style = style;
+
+            head.value_type = value_type;
+            head.name_len = (ushort)(name_length);
+            head.value_len = (ushort)(value_length);
+
+            IntPtr headPtr = Marshal.AllocHGlobal(headSize);
+            Marshal.StructureToPtr(head, headPtr, false);
+            Marshal.Copy(headPtr, buf, 0, headSize);
+            nameBytes.CopyTo(buf, headSize);
+            buf[headSize + nameBytes.Length] = 0; //add '\0'
+            valueBytes.CopyTo(buf, headSize + head.name_len);
+            Marshal.FreeHGlobal(headPtr);
+
+            ctx.socket.Send(buf, 0, final_length, SocketFlags.DontRoute);
         }
-        static public void Value(string name, byte value)
+        static public void Value(uint style, string name, sbyte value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name, 
+                AXV_UINT8, 
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, short value)
+        static public void Value(uint style, string name, byte value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_INT8,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, ushort value)
+        static public void Value(uint style, string name, short value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_INT16,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, int value)
+        static public void Value(uint style, string name, ushort value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_UINT16,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, uint value)
+        static public void Value(uint style, string name, int value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_INT32,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, long value)
+        static public void Value(uint style, string name, uint value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_UINT32,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, ulong value)
+        static public void Value(uint style, string name, long value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_INT64,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, float value)
+        static public void Value(uint style, string name, ulong value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_UINT64,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, double value)
+        static public void Value(uint style, string name, float value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_FLOAT32,
+                BitConverter.GetBytes(value));
         }
-        static public void Value(string name, string value)
+        static public void Value(uint style, string name, double value)
         {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            _value(ctx, style, name,
+                AXV_FLOAT64,
+                BitConverter.GetBytes(value));
+        }
+        static public void Value(uint style, string name, string value)
+        {
+            Context ctx = _getContext();
+            if (!ctx.init_success) return;
+
+            byte[] stringBytes = System.Text.Encoding.UTF8.GetBytes(value);
+            byte[] withNull = new byte[stringBytes.Length+1];
+            stringBytes.CopyTo(withNull, 0);
+            withNull[stringBytes.Length] = 0;
+            _value(ctx, style, name,
+                AXV_STR_UTF8,
+                withNull);
         }
     }
 
--- test.cs/Program.cs
+++ test.cs/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using com.thecodeway;
 
 namespace test.win32.cs
@@ -13,9 +14,41 @@ namespace test.win32.cs
         {
             System.Console.WriteLine("============= AxTrace3 Test(C#) ================\n");
 
-            AxTrace.Trace(AxTrace.AXT_TRACE, "-=-=-=-=-=-= Hello,World -=-=-=-=-=-=-=-=-=-");
-            AxTrace.Trace(AxTrace.AXT_TRACE, "中文字符+Ascii");
-            AxTrace.Trace(AxTrace.AXT_TRACE, "MultiLineTest\nLine{0}:第一行\nLine2:{1}\nLine3:第三行pi={2}", 1, "第二行", 3.14f);
+            {
+                AxTrace.Trace(AxTrace.AXT_TRACE, "-=-=-=-=-=-= Hello,World -=-=-=-=-=-=-=-=-=-");
+                AxTrace.Trace(AxTrace.AXT_TRACE, "中文字符+Ascii");
+                AxTrace.Trace(AxTrace.AXT_TRACE, "MultiLineTest\nLine{0}:第一行\nLine2:{1}\nLine3:第三行pi={2}", 1, "第二行", 3.14f);
+            }
+
+            {
+                //test value
+                AxTrace.Value(AxTrace.AXT_TRACE, "Int_Test", (int)-12345);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "BYTE_MAX_VALUE", (byte)byte.MaxValue);
+                AxTrace.Value(AxTrace.AXT_TRACE, "BYTE_MIN_VALUE", (byte)byte.MinValue);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "SHORT_MAX_VALUE", (short)short.MaxValue);
+                AxTrace.Value(AxTrace.AXT_TRACE, "SHORT_MIN_VALUE", (short)short.MinValue);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "INT_MAX_VALUE", (int)int.MaxValue);
+                AxTrace.Value(AxTrace.AXT_TRACE, "INT_MIN_VALUE", (int)int.MinValue);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "LONG_MAX_VALUE", (long)long.MaxValue);
+                AxTrace.Value(AxTrace.AXT_TRACE, "LONG_MIN_VALUE", (long)long.MinValue);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "Float_Test", (float)-3.1415926f);
+                AxTrace.Value(AxTrace.AXT_TRACE, "Double_Test", (double)-3.141592653589793238462643383279);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "FLOAT_MAX_VALUE", (float)float.MaxValue);
+                AxTrace.Value(AxTrace.AXT_TRACE, "FLOAT_MIN_VALUE", (float)float.MinValue);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "DOUBLE_MAX_VALUE", (double)double.MaxValue);
+                AxTrace.Value(AxTrace.AXT_TRACE, "DOUBLE_MIN_VALUE", (double)double.MinValue);
+
+                AxTrace.Value(AxTrace.AXT_TRACE, "String_Test", "String 汉字 123");
+            }
+            //TODO: remove this line
+            Thread.Sleep(1000);
         }
     }
 }
