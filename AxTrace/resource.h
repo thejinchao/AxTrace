1816836f7fd0d35407d6adce1750baf8420926f9
thejinchao <thejinchao@gmail.com>
2016-05-21 20:28:53 +0800

    增加快捷键设置

--- AxTrace/resource.h
+++ AxTrace/resource.h
@@ -49,7 +49,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        116
-#define _APS_NEXT_COMMAND_VALUE         40034
+#define _APS_NEXT_COMMAND_VALUE         40037
 #define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
