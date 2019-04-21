fb55b09989782aac68f66bd46d9f1b76f83909e4
thejinchao <thejinchao@gmail.com>
2019-04-21 22:51:49 +0800

    Add Linux.C sample

---
+++ samples/linux.c/TestMain.cpp
@@ -0,0 +1,385 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <limits.h>
+#include <float.h>
+#include <math.h>
+
+#include "ctpl.h"
+#include "axtrace.linux.h"
+
+int press_any_key(const char* message)
+{
+    
+	int c = 0;
+	struct termios org_opts, new_opts;
+	int res = 0;
+    printf("%s\n", message);
+	//-----  store old settings -----------
+	res = tcgetattr(STDIN_FILENO, &org_opts);
+	//---- set new terminal parms --------
+	memcpy(&new_opts, &org_opts, sizeof(new_opts));
+	new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
+	tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
+	c = getchar();
+	//------  restore old settings ---------
+	res = tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+	return c;
+}
+
+//--------------------------------------------------------------------------------------------
+struct AxTraceRequest
+{
+	void operator()(int id)
+	{
+		axlog(AXT_TRACE, "[%d]%s", id, message.c_str());
+	}
+
+	std::string message;
+
+	AxTraceRequest(const char* msg) : message(msg) {}
+};
+
+//--------------------------------------------------------------------------------------------
+struct  AxValueRequest
+{
+	void operator()(int id)
+	{
+		axvalue(AXV_STR_UTF32, m_strName.c_str(), m_strValue.c_str());
+	}
+
+	std::string m_strName;
+	std::wstring m_strValue;
+
+	AxValueRequest(const char* szValueName, const wchar_t* wszVlaue) :m_strName(szValueName), m_strValue(wszVlaue) {}
+};
+
+//--------------------------------------------------------------------------------------------
+double rand_number(double r1, double r2)
+{
+	double min = r1 < r2 ? r1 : r2;
+	double range = abs(r1 - r2);
+	return rand()*range / RAND_MAX + min;
+}
+
+//--------------------------------------------------------------------------------------------
+int main(int argc, char* argv[])
+{
+    printf("============= AxTrace4 Linux Test(C++) ================\n");
+
+    {
+        axlog(AXT_TRACE, "-=-=-=-=-=-= Hello,World -=-=-=-=-=-=-=-=-=-");
+        axlog(AXT_TRACE, "中文字符+Ascii");
+        axlog(AXT_TRACE, "MultiLineTest\nLine1:第一行\nLine2:第二行\nLine%d:第三行", 3);
+
+		//test axlog type
+        axlog(AXT_DEBUG, "DEBUG: This is a debug message");
+        axlog(AXT_INFO, "INFO: This is a info message");
+        axlog(AXT_WARN, "WARN: This is a warning message");
+        axlog(AXT_ERROR, "ERROR: This is a error message");
+        axlog(AXT_FATAL, "FATAL: This is a fatal message");
+    }
+
+	//--------------------------
+	{
+		press_any_key("AxTrace Pressure Test");
+
+		int blank_Count = 0;
+		int step = 1;
+		int MAX_BLANK_COUNT = 50;
+
+		axlog(AXT_TRACE, "<BEGIN>");
+		for (int i = 0; i<500; i++)
+		{
+			char szTemp[1024] = { 0 };
+
+			int j = 0;
+			for (j = 0; j<blank_Count; j++) szTemp[j] = L' ';
+
+			blank_Count += step;
+			if (blank_Count >= MAX_BLANK_COUNT) step = -1;
+			if (blank_Count <= 0)step = 1;
+
+			szTemp[j++] = L'*';
+			szTemp[j++] = 0;
+
+			axlog(AXT_TRACE, szTemp);
+		}
+		axlog(AXT_TRACE, "<END>");
+	}
+
+	//--------------------------
+	{
+		press_any_key("AxTrace Multithread Pressure Test");
+
+		ctpl::thread_pool tp(std::thread::hardware_concurrency());
+
+		int blank_Count = 0;
+		int step = 1;
+		int MAX_BLANK_COUNT = 50;
+
+		for (int i = 0; i < 500; i++)
+		{
+			char szTemp[1024] = { 0 };
+
+			int j = 0;
+			for (j = 0; j < blank_Count; j++) szTemp[j] = L' ';
+
+			blank_Count += step;
+			if (blank_Count >= MAX_BLANK_COUNT) step = -1;
+			if (blank_Count <= 0)step = 1;
+
+			szTemp[j++] = L'*';
+			szTemp[j++] = 0;
+
+			tp.push(AxTraceRequest(szTemp));
+		}
+
+	}	
+
+	//--------------------------
+	{
+		press_any_key("AxValue Test");
+
+		#define AXVALUE(name, ex, type, out_type, value) name=value; axvalue(type, #name ex, &name); printf(#name ex "=" out_type "\n", value);
+
+		char int_8 = 0;
+		AXVALUE(int_8, "_0", AXV_INT8, "%d", 0);
+		AXVALUE(int_8, "_MIN", AXV_INT8, "%d", SCHAR_MIN);
+		AXVALUE(int_8, "_MAX", AXV_INT8, "%d", SCHAR_MAX);
+
+		unsigned int uint_8 = 0;
+		AXVALUE(uint_8, "_MIN", AXV_UINT8, "%d", 0);
+		AXVALUE(uint_8, "_MAX", AXV_UINT8, "%d", UCHAR_MAX);
+
+		short int_16 = 0;
+		AXVALUE(int_16, "_0", AXV_INT16, "%d", 0);
+		AXVALUE(int_16, "_MIN", AXV_INT16, "%d", SHRT_MIN);
+		AXVALUE(int_16, "_MAX", AXV_INT16, "%d", SHRT_MAX);
+
+		unsigned short uint_16 = 0;
+		AXVALUE(uint_16, "_MIN", AXV_UINT16, "%d", 0);
+		AXVALUE(uint_16, "_MAX", AXV_UINT16, "%d", USHRT_MAX);
+
+		int int_32 = 8;
+		AXVALUE(int_32, "_0", AXV_INT32, "%d", 0);
+		AXVALUE(int_32, "_MIN", AXV_INT32, "%d", INT_MIN);
+		AXVALUE(int_32, "_MAX", AXV_INT32, "%d", INT_MAX);
+
+		unsigned int uint_32 = 0;
+		AXVALUE(uint_32, "_MIN", AXV_UINT32, "%d", 0);
+		AXVALUE(uint_32, "_MAX", AXV_UINT32, "%u", UINT_MAX);
+
+		long long int_64 = 0;
+		AXVALUE(int_64, "_0", AXV_INT64, "%lld", 0LL);
+		AXVALUE(int_64, "_MIN", AXV_INT64, "%lld", LLONG_MIN);
+		AXVALUE(int_64, "_MAX", AXV_INT64, "%lld", LLONG_MAX);
+
+		unsigned long long uint_64 = 0;
+		AXVALUE(uint_64, "_MIN", AXV_UINT64, "%llu", 0LL);
+		AXVALUE(uint_64, "_MAX", AXV_UINT64, "%llu", ULLONG_MAX);
+
+		float float_32 = 0.f;
+		AXVALUE(float_32, "_0", AXV_FLOAT32, "%f", 0.f);
+		AXVALUE(float_32, "_MAX", AXV_FLOAT32, "%e", FLT_MAX);
+		AXVALUE(float_32, "_MIN", AXV_FLOAT32, "%e", FLT_MIN);
+
+		double double_64 = 0.0;
+		AXVALUE(double_64, "_0", AXV_FLOAT64, "%f", 0.0);
+		AXVALUE(double_64, "_MAX", AXV_FLOAT64, "%e", DBL_MAX);
+		AXVALUE(double_64, "_MIN", AXV_FLOAT64, "%e", DBL_MIN);
+
+		const char* pszString_UTF8 = "String \xE6\xB1\x89\xE5\xAD\x97(UTF8)"; //String 汉字（utf8)
+		axvalue(AXV_STR_UTF8, "String_UTF8", pszString_UTF8);
+
+		const wchar_t* wszString = L"String 汉字(UTF32)";
+		axvalue(AXV_STR_UTF32, "String_UTF32", wszString);
+	}	
+	
+	//--------------------------
+	{
+		press_any_key("AxValue Pressure Test");
+
+		int start_blank = 0;
+		int start_step = 1;
+		int MAX_BLANK_COUNT = 50;
+
+		for (int i = 0; i < 100; i++)
+		{
+			axvalue(AXV_INT32, "start_blank", &start_blank);
+
+			int blank_Count = start_blank;
+			int step = start_step;
+
+			for (int j = 0; j < 50; j++)
+			{
+				wchar_t wszTemp[1024] = { 0 };
+
+				int k = 0;
+				for (k = 0; k < blank_Count; k++) wszTemp[k] = L' ';
+
+				blank_Count += step;
+				if (blank_Count >= MAX_BLANK_COUNT) step = -1;
+				if (blank_Count <= 0)step = 1;
+
+				wszTemp[k++] = L'*';
+				wszTemp[k++] = 0;
+
+				char value_name[32] = { 0 };
+				snprintf(value_name, 32, "Value_%02d", j);
+				axvalue(AXV_STR_UTF32, value_name, wszTemp);
+			}
+
+			start_blank += start_step;
+			if (start_blank >= MAX_BLANK_COUNT) start_step = -1;
+			if (start_blank <= 0) start_step = 1;
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+	}	
+	
+	{
+	    press_any_key("AxValue Multi Thread Pressure Test");
+	    
+		ctpl::thread_pool tp(std::thread::hardware_concurrency());
+
+		int start_blank = 0;
+		int start_step = 1;
+		int MAX_BLANK_COUNT = 50;
+
+		for (int i = 0; i<500; i++)
+		{
+			axvalue(AXV_INT32, "start_blank_multiThread", &start_blank);
+			int blank_Count = start_blank;
+			int step = start_step;
+
+			for (int j = 0; j<50; j++)
+			{
+				wchar_t wszTemp[1024] = { 0 };
+
+				int k = 0;
+				for (k = 0; k<blank_Count; k++) wszTemp[k] = L' ';
+
+				blank_Count += step;
+				if (blank_Count >= MAX_BLANK_COUNT) step = -1;
+				if (blank_Count <= 0)step = 1;
+
+				wszTemp[k++] = L'*';
+				wszTemp[k++] = 0;
+
+				char value_name[32] = { 0 };
+				snprintf(value_name, 32, "Value_%02d", j);
+
+				tp.push(AxValueRequest(value_name, wszTemp));
+			}
+
+			start_blank += start_step;
+			if (start_blank >= MAX_BLANK_COUNT) start_step = -1;
+			if (start_blank <= 0) start_step = 1;
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+
+	}
+
+	{
+		press_any_key("AxMap2D Test");
+
+		const int ACTOR_COUNTS = 100;
+		const int MOVE_COUNTS = 200;
+
+		const double MAP_LEFT = -260.0;
+		const double MAP_TOP = 256.0;
+		const double MAP_RIGHT = 256.0;
+		const double MAP_BOTTOM = -256.0;
+		const double MOVE_STEP = 2.0;
+		const double PI = 3.14159265;
+
+		double MAP_WIDTH = MAP_RIGHT - MAP_LEFT;
+		double MAP_HEIGHT = MAP_BOTTOM - MAP_TOP;	    
+		
+		struct Actor
+		{
+			double sx, sy;
+			double tx, ty;
+
+			double x, y;
+			double dir;
+
+			double current_distance(void) const {
+				return sqrt((x - sx)*(x - sx) + (y - sy)*(y - sy));
+			}
+			double remain_distance(void) const {
+				return sqrt((x - tx)*(x - tx) + (y - ty)*(y - ty));
+			}
+		};
+
+		Actor allActors[ACTOR_COUNTS];
+	
+		std::function<void(Actor&)> _selectNextTarget = [&](Actor& actor) {
+			do {
+				actor.tx = rand_number(MAP_LEFT, MAP_RIGHT);
+				actor.ty = rand_number(MAP_TOP, MAP_BOTTOM);
+			} while (actor.remain_distance()< MOVE_STEP*10);
+			actor.sx = actor.x;
+			actor.sy = actor.y;
+			actor.dir = atan2(actor.ty-actor.sy, actor.tx-actor.sx);
+		};
+		
+		for (int i = 0; i < ACTOR_COUNTS; i++)
+		{
+			Actor& actor = allActors[i];
+
+			actor.sx = actor.x = rand_number(MAP_LEFT, MAP_RIGHT);
+			actor.sy = actor.y = rand_number(MAP_TOP, MAP_BOTTOM);
+			_selectNextTarget(actor);
+		}	
+		
+		for (int i = 0; i < MOVE_COUNTS; i++)
+		{
+			ax2d_begin_scene("test", MAP_LEFT, MAP_TOP, MAP_RIGHT, MAP_BOTTOM, "{\"gridSize\":[32.0,32.0], \"gridPoint\":[-256.0, 256.0]}");
+
+			for (int j = 0; j < ACTOR_COUNTS; j++)
+			{
+				Actor& actor = allActors[j];
+
+				ax2d_actor("test", 100 + j, actor.x, actor.y, actor.dir, 0);
+
+				if (actor.remain_distance() <= MOVE_STEP) {
+					_selectNextTarget(actor);
+				}
+
+				if ((rand() % 10) > 5) {
+					double distance = actor.current_distance() + MOVE_STEP;
+					actor.x = actor.sx + distance * cos(actor.dir);
+					actor.y = actor.sy + distance * sin(actor.dir);
+				}
+			}
+			ax2d_end_scene("test");
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+		
+        press_any_key("Remove Actor Test");
+        
+		for (int i = 0; i < ACTOR_COUNTS; i++)
+		{
+			char temp[256] = { 0 };
+			double gridPointX = -256.0;
+			double gridPointY = 2560;
+			snprintf(temp, 256, "{\"gridSize\":[32.0,32.0], \"gridPoint\":[%f,%f]}", gridPointX+i, gridPointY-i);
+
+			ax2d_begin_scene("test", MAP_LEFT * 2, MAP_TOP * 2, MAP_RIGHT * 2, MAP_BOTTOM * 2, temp);
+			for (int j = 0; j < ACTOR_COUNTS-i; j++)
+			{
+				Actor& actor = allActors[j];
+				ax2d_actor("test", 100 + j, actor.x, actor.y, actor.dir, 0);
+			}
+			ax2d_end_scene("test");
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		}		
+	}
+	
+}
---
+++ samples/linux.c/axtrace.linux.c
@@ -0,0 +1,447 @@
+#include "axtrace.linux.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <wchar.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/syscall.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+
+/*---------------------------------------------------------------------------------------------*/
+#define DEFAULT_AXTRACE_SERVER_IP		"127.0.0.1"
+#define DEFAULT_AXTRACE_SERVER_PORT		(1978)
+#define AXTRACE_MAX_TRACE_STRING_LENGTH	(0x8000)
+#define AXTRACE_MAX_VALUENAME_LENGTH	(128)
+#define AXTRACE_MAX_VALUE_LENGTH		(1024)
+#define AXTRACE_MAX_SCENE_NAME_LENGTH	(128)
+#define AXTRACE_MAX_SCENE_DEFINE_LENGTH	(2048)
+
+#define AXTRACE_CMD_TYPE_LOG			(1)
+#define AXTRACE_CMD_TYPE_VALUE			(2)
+#define AXTRACE_CMD_TYPE_2D_BEGIN_SCENE	(3)
+#define AXTRACE_CMD_TYPE_2D_ACTOR		(4)
+#define AXTRACE_CMD_TYPE_2D_END_SCENE	(5)
+
+/*---------------------------------------------------------------------------------------------*/
+/* AxTrace Global data  */
+typedef struct
+{
+	int		is_init_succ;			/* 0 means not, 1 means yes*/
+	struct	sockaddr_in address;		/* axtrace server address */
+	int		sfd;					/* socket file desc*/
+} axtrace_contex_s;
+
+/*---------------------------------------------------------------------------------------------*/
+#pragma pack(push)
+#pragma pack(1)
+/* axtrace communication data struct*/
+typedef struct
+{
+	unsigned short	length;			/* length */
+	unsigned char	flag;			/* magic flag, always 'A' */
+	unsigned char	type;			/* command type AXTRACE_CMD_TYPE_* */
+	unsigned int	pid;			/* process id*/
+	unsigned int	tid;			/* thread id*/
+} axtrace_head_s;
+
+/* axtrace log data struct*/
+typedef struct
+{
+	axtrace_head_s	head;			/* common head */
+	unsigned int	log_type;		/* trace style AXT_* */
+	unsigned short	code_page;		/* code page */
+	unsigned short	length;			/* trace string length */
+
+									/* [trace string data with '\0' ended] */
+} axtrace_log_s;
+
+typedef struct
+{
+	axtrace_head_s	head;			/* common head */
+	unsigned int	value_type;		/* value type AXV_* */
+	unsigned short	name_len;		/* length of value name */
+	unsigned short	value_len;		/* length of value */
+
+	/* [name buf  with '\0' ended]*/
+	/* [value buf] */
+} axtrace_value_s;
+
+typedef struct
+{
+	axtrace_head_s	head;			/* common head */
+	double			left;			/* left of scene*/
+	double			top;			/* top of scene*/
+	double			right;			/* right of scene*/
+	double			bottom;			/* bottom of scene*/
+	unsigned short	name_len;		/* length of scene name */
+	unsigned short	define_len;		/* length of scene define */
+
+									/* [scene name buf  with '\0' ended]*/
+									/* [scene define buf  with '\0' ended]*/
+} axtrace_2d_begin_scene_s;
+
+typedef struct
+{
+	axtrace_head_s	head;			/* common head */
+	long long		actor_id;		/* id of actor */
+	double			x;				/* position (x)*/
+	double			y;				/* position (y)*/
+	double			dir;			/* direction */
+	unsigned int	style;			/* user define style */
+	unsigned short	name_len;		/* length of scene name */
+
+									/* [scene name buf  with '\0' ended]*/
+} axtrace_2d_actor_s;
+
+typedef struct
+{
+	axtrace_head_s	head;			/* common head */
+	unsigned short	name_len;		/* length of scene name */
+
+									/* [scene name buf  with '\0' ended]*/
+} axtrace_2d_end_scene_s;
+
+#pragma pack(pop)
+
+/*---------------------------------------------------------------------------------------------*/
+static axtrace_contex_s* _axtrace_try_init(const char* server_ip, unsigned short server_port)
+{
+    const char* final_server_ip = server_ip ? server_ip : DEFAULT_AXTRACE_SERVER_IP;
+    unsigned short findal_server_port = server_port != 0 ? server_port : DEFAULT_AXTRACE_SERVER_PORT;
+    
+	axtrace_contex_s* ctx = (axtrace_contex_s*)malloc(sizeof(axtrace_contex_s));
+	if (ctx == 0){
+		//TODO: fatal error, should stop the process
+		return 0;
+	}
+	memset(ctx, 0, sizeof(axtrace_contex_s));
+	
+	/* create address struct */
+	ctx->address.sin_family = AF_INET;
+	ctx->address.sin_port = htons(server_port != 0 ? server_port : DEFAULT_AXTRACE_SERVER_PORT);
+	if (inet_pton(AF_INET, server_ip ? server_ip : DEFAULT_AXTRACE_SERVER_IP, &(ctx->address.sin_addr)) != 1) {
+		return ctx;
+	}
+	
+	/* create socket */
+	ctx->sfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (ctx->sfd < 0) {
+		return ctx;
+	}	
+	
+	/* set SO_LINGER off, make sure all data in send buf can be sended */
+	struct linger linger_;
+	linger_.l_onoff = 0;
+	linger_.l_linger = 0;
+	setsockopt(ctx->sfd, SOL_SOCKET, SO_LINGER, (const void*)&linger_, sizeof(linger_));
+	
+	/* connect to axtrace server */
+	if (connect(ctx->sfd, (const struct sockaddr*)&(ctx->address), sizeof(struct sockaddr_in)) < 0) {
+		close(ctx->sfd); 
+		ctx->sfd = 0;
+		return ctx;
+	}
+	/* init success */
+	ctx->is_init_succ = 1;
+	return ctx;
+}
+
+/*---------------------------------------------------------------------------------------------*/
+static axtrace_contex_s* _axtrace_get_thread_contex(const char* server_ip, unsigned short server_port)
+{
+	static __thread axtrace_contex_s* s_the_thread_data = 0;
+
+	if (s_the_thread_data != 0) {
+		/* already try init in this thread */
+		return (s_the_thread_data->is_init_succ != 0) ? s_the_thread_data : 0;
+	}
+
+	/* try init */
+	s_the_thread_data = _axtrace_try_init(server_ip, server_port);
+	return (s_the_thread_data->is_init_succ != 0) ? s_the_thread_data : 0;	
+}
+
+/*---------------------------------------------------------------------------------------------*/
+void axlog(unsigned int log_type, const char *format, ...)
+{
+	axtrace_contex_s* ctx;
+	va_list ptr;
+	size_t contents_byte_size, final_length;
+	int send_len;
+	    
+	/* buf for send , call send() once*/
+	char buf[sizeof(axtrace_log_s) + AXTRACE_MAX_TRACE_STRING_LENGTH] = { 0 };
+	axtrace_log_s* trace_head = (axtrace_log_s*)(buf);
+	char* trace_string = (char*)(buf + sizeof(axtrace_log_s));
+
+	/* is init ok? */
+	ctx = _axtrace_get_thread_contex(0, 0);
+	if (ctx == 0) return;
+
+	/* Create String Contents*/
+	va_start(ptr, format);
+	contents_byte_size = vsnprintf(trace_string, AXTRACE_MAX_TRACE_STRING_LENGTH, format, ptr);
+	va_end(ptr);
+	/* failed ?*/
+	if (contents_byte_size < 0 ) return;	
+	
+	/* add '\0' ended */
+	contents_byte_size += 1;
+
+	/* fill the trace head data */
+	final_length = sizeof(axtrace_log_s) + contents_byte_size;
+
+	trace_head->head.length = (unsigned short)(final_length);
+	trace_head->head.flag = 'A';
+	trace_head->head.type = AXTRACE_CMD_TYPE_LOG;
+	trace_head->head.pid = getpid();
+	trace_head->head.tid = syscall(SYS_gettid);
+
+	trace_head->log_type = log_type;
+	trace_head->code_page = ATC_UTF8;	/* TODO: get current system code page*/
+	trace_head->length = (unsigned short)contents_byte_size;
+
+	/* send to axtrace server*/
+	send_len = write(ctx->sfd, buf, (int)final_length);
+	
+	/*TODO: check result, may be reconnect to server */
+	return;
+}
+
+/*---------------------------------------------------------------------------------------------*/
+static size_t _get_value_length(unsigned int value_type, const void* value)
+{
+	size_t l;
+
+	switch (value_type)
+	{
+	case AXV_INT8: case AXV_UINT8:  return 1;
+	case AXV_INT16: case AXV_UINT16: return 2;
+	case AXV_INT32: case AXV_UINT32: return 4;
+	case AXV_INT64: case AXV_UINT64: return 8;
+	case AXV_FLOAT32: return 4;
+	case AXV_FLOAT64: return 8;
+	case AXV_STR_UTF8:
+		{
+			l = strnlen((const char*)value, AXTRACE_MAX_VALUE_LENGTH - 1);
+			return l + 1;
+		}
+	case AXV_STR_UTF32:
+		{
+			l = wcsnlen((const wchar_t*)value, AXTRACE_MAX_VALUE_LENGTH/sizeof(wchar_t)-1);
+			return (l + 1)*sizeof(wchar_t);
+		}
+	default: break;
+	}
+	return -1;
+}
+
+/*---------------------------------------------------------------------------------------------*/
+void axvalue(unsigned int value_type, const char* value_name, const void* value)
+{
+	axtrace_contex_s* ctx;
+	size_t value_name_length;
+	size_t value_length;
+	int send_len;
+	size_t final_length;
+	unsigned short* temp;
+
+	/* buf for send , call send() once*/
+	char buf[sizeof(axtrace_value_s) + AXTRACE_MAX_VALUENAME_LENGTH + AXTRACE_MAX_VALUE_LENGTH] = { 0 };
+	axtrace_value_s* trace_head = (axtrace_value_s*)(buf);
+	char* value_name_buf = (char*)(buf + sizeof(axtrace_value_s));
+
+	/* is init ok? */
+	ctx = _axtrace_get_thread_contex(0, 0);
+	if (ctx == 0) return;
+
+	/** get value name length*/
+	if (value_name == 0) return;
+	value_name_length = strnlen(value_name, AXTRACE_MAX_VALUENAME_LENGTH - 1);
+	/* add '\0' ended */
+	value_name_length += 1;
+	if (value_name_length <= 0 || value_name_length >= AXTRACE_MAX_VALUENAME_LENGTH) return;
+
+	if (value == 0) return;
+	value_length = _get_value_length(value_type, value);
+	if (value_length <= 0 || value_length >= AXTRACE_MAX_VALUE_LENGTH) return;
+
+	/*calc final length */
+	final_length = sizeof(axtrace_value_s) + value_name_length + value_length;
+
+	trace_head->head.length = (unsigned short)(final_length);
+	trace_head->head.flag = 'A';
+	trace_head->head.type = AXTRACE_CMD_TYPE_VALUE;
+	trace_head->head.pid = getpid();
+	trace_head->head.tid = syscall(SYS_gettid);
+
+	trace_head->value_type = value_type;
+	trace_head->name_len = (unsigned short)value_name_length;
+	trace_head->value_len = (unsigned short)value_length;
+
+	/* fill the value data */
+	memcpy(value_name_buf, value_name, value_name_length);
+	memcpy(value_name_buf + value_name_length, value, value_length);
+
+	/* send to axtrace server*/
+	send_len = write(ctx->sfd, buf, (int)final_length);
+
+	/*TODO: check result, may be reconnect to server */
+	return;
+}
+
+/*---------------------------------------------------------------------------------------------*/
+void ax2d_begin_scene(const char* scene_name, double left, double top, double right, double bottom, const char* scene_define)
+{
+    axtrace_contex_s* ctx;
+	size_t scene_name_size, scene_define_size, final_length;
+	int send_len;
+	
+	/* buf for send , call send() once*/
+	char buf[sizeof(axtrace_2d_begin_scene_s) + AXTRACE_MAX_SCENE_NAME_LENGTH + AXTRACE_MAX_SCENE_DEFINE_LENGTH] = { 0 };
+	axtrace_2d_begin_scene_s* trace_head = (axtrace_2d_begin_scene_s*)(buf);
+	char* _name = (char*)(buf + sizeof(axtrace_2d_begin_scene_s));
+
+	/* is init ok? */
+	ctx = _axtrace_get_thread_contex(0, 0);
+	if (ctx == 0) return;
+
+	/** get scene name length*/
+	if (scene_name == 0) return;
+	scene_name_size = strnlen(scene_name, AXTRACE_MAX_SCENE_NAME_LENGTH - 1);
+	/* add '\0' ended */
+	scene_name_size += 1;
+	if (scene_name_size <= 0 || scene_name_size >= AXTRACE_MAX_SCENE_NAME_LENGTH) return;
+    memcpy(_name, scene_name, scene_name_size);
+    
+	/* to scene define point */
+	if (scene_define != 0) {
+		_name = (char*)(buf + sizeof(axtrace_2d_begin_scene_s) + scene_name_size);
+        
+        /** get scene define length */
+        scene_define_size = strnlen(scene_define, AXTRACE_MAX_SCENE_DEFINE_LENGTH - 1);
+        scene_define_size += 1;
+
+		/* add '\0' ended */
+		scene_define_size += 1;
+		if (scene_define_size >= AXTRACE_MAX_SCENE_DEFINE_LENGTH) return;
+		    
+		memcpy(_name, scene_define, scene_define_size);
+	}
+	else {
+		scene_define_size = 0;
+	}
+	
+	final_length = sizeof(axtrace_2d_begin_scene_s) + scene_name_size + scene_define_size;
+
+	trace_head->head.length = (unsigned short)(final_length);
+	trace_head->head.flag = 'A';
+	trace_head->head.type = AXTRACE_CMD_TYPE_2D_BEGIN_SCENE;
+	trace_head->head.pid = getpid();
+	trace_head->head.tid = syscall(SYS_gettid);
+
+	trace_head->left = left;
+	trace_head->top = top;
+	trace_head->right = right;
+	trace_head->bottom = bottom;
+	trace_head->name_len = (unsigned short)scene_name_size;
+	trace_head->define_len = (unsigned short)scene_define_size;
+
+	/* send to axtrace server*/
+	send_len = write(ctx->sfd, buf, (int)final_length);	
+	
+	return;
+}
+
+/*---------------------------------------------------------------------------------------------*/
+void ax2d_actor(const char* scene_name, long long actor_id, double x, double y, double dir, unsigned int actor_style)
+{
+    axtrace_contex_s* ctx;
+	size_t scene_name_size, scene_define_size, final_length;
+	int send_len;
+
+	/* buf for send , call send() once*/
+	char buf[sizeof(axtrace_2d_actor_s) + AXTRACE_MAX_SCENE_NAME_LENGTH] = { 0 };
+	axtrace_2d_actor_s* trace_head = (axtrace_2d_actor_s*)(buf);
+	char* _name = (char*)(buf + sizeof(axtrace_2d_actor_s));	    
+    
+	/* is init ok? */
+	ctx = _axtrace_get_thread_contex(0, 0);
+	if (ctx == 0) return;
+	        
+	/** get scene name length*/
+	if (scene_name == 0) return;
+	scene_name_size = strnlen(scene_name, AXTRACE_MAX_SCENE_NAME_LENGTH - 1);
+	/* add '\0' ended */
+	scene_name_size += 1;
+	if (scene_name_size <= 0 || scene_name_size >= AXTRACE_MAX_SCENE_NAME_LENGTH) return;
+    memcpy(_name, scene_name, scene_name_size);
+    
+	final_length = sizeof(axtrace_2d_actor_s) + scene_name_size;
+
+	trace_head->head.length = (unsigned short)(final_length);
+	trace_head->head.flag = 'A';
+	trace_head->head.type = AXTRACE_CMD_TYPE_2D_ACTOR;
+	trace_head->head.pid = getpid();
+	trace_head->head.tid = syscall(SYS_gettid);
+
+	trace_head->actor_id = actor_id;
+	trace_head->x = x;
+	trace_head->y = y;
+	trace_head->dir = dir;
+	trace_head->style = actor_style;
+	trace_head->name_len = (unsigned short)scene_name_size;
+	
+	/* send to axtrace server*/
+	send_len = write(ctx->sfd, buf, (int)final_length);	
+	
+	return;
+}
+
+/*---------------------------------------------------------------------------------------------*/
+void ax2d_end_scene(const char* scene_name)
+{
+    axtrace_contex_s* ctx;
+	size_t scene_name_size, scene_define_size, final_length;
+	int send_len;
+	
+	/* buf for send , call send() once*/
+	char buf[sizeof(axtrace_2d_end_scene_s) + AXTRACE_MAX_SCENE_NAME_LENGTH] = { 0 };
+	axtrace_2d_end_scene_s* trace_head = (axtrace_2d_end_scene_s*)(buf);
+	char* _name = (char*)(buf + sizeof(axtrace_2d_end_scene_s));	    
+    
+    /* is init ok? */
+	ctx = _axtrace_get_thread_contex(0, 0);
+	if (ctx == 0) return;
+    
+	/** get scene name length*/
+	if (scene_name == 0) return;
+	scene_name_size = strnlen(scene_name, AXTRACE_MAX_SCENE_NAME_LENGTH - 1);
+	/* add '\0' ended */
+	scene_name_size += 1;
+	if (scene_name_size <= 0 || scene_name_size >= AXTRACE_MAX_SCENE_NAME_LENGTH) return;
+    memcpy(_name, scene_name, scene_name_size);
+    
+	final_length = sizeof(axtrace_2d_end_scene_s) + scene_name_size;
+
+	trace_head->head.length = (unsigned short)(final_length);
+	trace_head->head.flag = 'A';
+	trace_head->head.type = AXTRACE_CMD_TYPE_2D_END_SCENE;
+	trace_head->head.pid = getpid();
+	trace_head->head.tid = syscall(SYS_gettid);
+
+	trace_head->name_len = (unsigned short)scene_name_size;
+	
+	/* send to axtrace server*/
+	send_len = write(ctx->sfd, buf, (int)final_length);	    
+    
+    return;
+}
